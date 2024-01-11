be5eef3ba73679d41b242e63d72db3d337e73369
marcvizca <marcvizca@gmail.com>
2024-01-11 22:27:47 +0100

    prueba en database

--- src/database.js
+++ src/database.js
@@ -1,15 +1,8 @@
-import {createPool} from 'mysql2/promise'
-import fs from 'fs'
-import dotenv from 'dotenv'
-//import * as path from 'path';
+import {createPool} from 'mysql2/promise';
+import fs from 'fs';
+import dotenv from 'dotenv';
 
-
-dotenv.config()
-
-//var path = require('path');
-//const __filename = new URL(import.meta.url).pathname;
-//const __dirname = path.dirname(__filename);
-//const certificatePath = path.join(__dirname, 'DigiCertGlobalRootCA.crt.pem');
+dotenv.config();
 export const pool = createPool(
     {
         host: process.env.MYSQL_HOST,
@@ -19,17 +12,13 @@ export const pool = createPool(
         port: process.env.MYSQL_PORT,
         ssl: {ca: fs.readFileSync('./src/DigiCertGlobalRootCA.crt.pem')}
     }
-)
+);
 
-/*export const pool = mysql.createConnection(config);
-
-pool.connect(
-    function (err) {
-        if (err) {
-            console.log("!!! Connot connect to database !!! Error:");
-            throw err;
-        } else {
-            console.log("Database connection established")
-        }
-    }
-);*/
+pool.getConnection()
+  .then(connection => {
+    console.log('Connected to database!');
+    connection.release();
+  })
+  .catch(error => {
+    console.error('Error connecting to database:', error);
+  });
